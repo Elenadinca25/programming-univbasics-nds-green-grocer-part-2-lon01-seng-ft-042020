f8cf559ad1ae7f3cd9a48aff813607d5eb1c3175
Elena Dinca <elenadinca25@gmail.com>
2020-04-15 18:14:52 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -25,15 +25,18 @@ def apply_coupons(cart, coupons)
   end
   cart
 end
-# Consult README for inputs and outputs
-#
-# REMEMBER: This method **should** update cart
+
 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+counter = 0
+while counter < cart.length
+  if cart[counter][:clearance]
+    cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
+    end
+  counter += 1
+  end
+  cart
 end
 
 def checkout(cart, coupons)
